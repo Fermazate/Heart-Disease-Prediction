f485995959758fb8d012508c554588fab97707d3
Fermazate <fernandomazate@gmail.com>
2023-09-29 08:16:37 -0600

    Reestructuración

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,4 +1,21 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import sys
+from sklearn.experimental import enable_iterative_imputer
+
+sys.path.append('/Users/andresgarciarobles/Documents/Proyectos de ciencia de datos/Heart-Disease-Prediction')
+
+#from src.features.build_features import Itimp, dropfew
+import src.features.build_features as bld
+
+df = pd.read_csv('data/raw/framingham.csv')
+
+#Drop rows with Nan values AND that represent <5% of the data.
+nona = bld.dropfew(df,'data/interim/dfnona.csv')
+nonaknn = bld.knn_replace(df,'data/interim/nonaknn.csv')
+nonaIt = bld.Itimp(df,'data/interim/nonait.csv')
+
+
+
+
 
--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,23 +1,42 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from sklearn.impute import KNNImputer
+from sklearn.impute import KNNImputer, IterativeImputer
 
 import warnings
 warnings.filterwarnings('ignore')
-#Open database.
-df = pd.read_csv('data/raw/framingham.csv')
 
-#Drop all the rows with missing values with no normal distributions and are less than 5% of total data.
-columns_withna = [var for var in df.columns if df[var].isnull().mean() < 0.05 and df[var].isnull().mean() > 0]
-df.dropna(subset=columns_withna, inplace=True)
+def dropfew(data, dir, save = True, ind = False):
+    #Drop all the rows with missing values with no normal distributions and are less than 5% of total data.
+    data = pd.DataFrame(data)
+    columns_withna = [var for var in data.columns if data[var].isnull().mean() < 0.05 and data[var].isnull().mean() > 0]
+    data.dropna(subset=columns_withna, inplace=True)
+    if save == True:
+        data.to_csv(dir,index = ind)
+    return data
+        
+def knn_replace (data, dir, save = True, ind = False):
+    #Replace missing values of glucose (Not <5% of total values) with multivariate imputation.  
+    data = pd.DataFrame(data)
+    knn_imp = KNNImputer(n_neighbors=2)
+    data = pd.DataFrame(knn_imp.fit_transform(data), columns=data.columns)
+    if save == True:
+        data.to_csv(dir,index=ind)
+    return data
 
-#Replace missing values of glucose (Not <5% of total values) with multivariate imputation.
-knn_imp = KNNImputer(n_neighbors=2)
-df2 = pd.DataFrame(knn_imp.fit_transform(df), columns=df.columns)
+def Itimp(data,dir,save = True,ind = False):
+    """_summary_
 
+    Args:
+        data (pandas.core.frame.DataFrame): A pandas database.
+        dir (string): Directory to save.
 
-#register new dataframe in an csv archive.
-df.to_csv('data/interim/dfnona.csv', index=False)
-df2.to_csv('data/interim/dfknn.csv', index=False)
-
+    Returns:
+        pandas.core.frame.DataFrame: Return a pandas dataframe with processed information
+    """
+    data = pd.DataFrame(data)
+    itimp = IterativeImputer()
+    data = pd.DataFrame(itimp.fit_transform(data),columns=data.columns)
+    if save == True:
+        data.to_csv(dir, index = ind)
+    return data
